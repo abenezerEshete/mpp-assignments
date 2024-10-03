b14d601c9151461e6451a82511ceebd00c75a236
ttiweht <hewitttusiime82@gmail.com>
2024-10-02 23:26:06 -0500

    remove commented code

--- assignments/lab2/problem_two/prob2B/outsidepackage/Main.java
+++ assignments/lab2/problem_two/prob2B/outsidepackage/Main.java
@@ -24,21 +24,6 @@ public class Main {
         }
     }
 
-//        OrderAndOrderLine orderManager = new OrderAndOrderLine();
-//
-//        // Create an Order
-//        Order order = orderManager.createOrder("Order01", LocalDate.now());
-//
-//        // Create additional OrderLines
-//        orderManager.createOrderLine(2, 10.49, 1, order);
-//        orderManager.createOrderLine(3, 5.09, 5, order);
-//        orderManager.createOrderLine(4, 15.00, 2, order);
-//        orderManager.createOrderLine(5, 8.99, 3, order);
-//
-//        // Display all orders and their lines
-//        orderManager.displayOrders();
-//    }
-   // }
-//}
+
 
 
