361b1b874c8197c82b7663f584ae69c91299b961
ttiweht <hewitttusiime82@gmail.com>
2024-10-02 23:30:37 -0500

    edited main

--- assignments/lab2/problem_two/prob2B/outsidepackage/Main.java
+++ assignments/lab2/problem_two/prob2B/outsidepackage/Main.java
@@ -1,28 +1,28 @@
 package prob2B.outsidepackage;//package Twob.outsidepackage
 
+import prob2B.Order;
+import prob2B.OrderAndOrderLine;
+
 import java.time.LocalDate;
-import Twob.Order;
-import Twob.OrderLine;
-import Twob.OrderAndOrderLine;
 
 
 public class Main {
-        public static void main(String[] args) {
-            OrderAndOrderLine orderManager = new OrderAndOrderLine();
+    public static void main(String[] args) {
+        OrderAndOrderLine orderManager = new OrderAndOrderLine();
 
-            // Create an Order
-            Order order = orderManager.createOrder("Order01", LocalDate.now());
+        // Create an Order
+        Order order = orderManager.createOrder("Order01", LocalDate.now());
 
-            // Create additional OrderLines
-            orderManager.createOrderLine(2, 10.49, 1, order);
-            orderManager.createOrderLine(3, 5.09, 5, order);
-            orderManager.createOrderLine(4, 15.00, 2, order);
-            orderManager.createOrderLine(5, 8.99, 3, order);
+        // Create additional OrderLines
+        orderManager.createOrderLine(2, 10.49, 1, order);
+        orderManager.createOrderLine(3, 5.09, 5, order);
+        orderManager.createOrderLine(4, 15.00, 2, order);
+        orderManager.createOrderLine(5, 8.99, 3, order);
 
-            // Display all orders and their lines
-            orderManager.displayOrders();
-        }
+        // Display all orders and their lines
+        orderManager.displayOrders();
     }
+}
 
 
 
